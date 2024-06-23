823a42064b7d44a93b5906990f6a47a1971f18df
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-06-23 17:30:11 +0530

    ui

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,13 +1,49 @@
-import React from 'react'
-import CollapsibleMenu from '../components/ui/CollapsibleMenu'
+import React from "react";
+import type { CollapseProps } from "antd";
+import { Collapse } from "antd";
+
+const items: CollapseProps["items"] = [
+  {
+    key: "1",
+    label: <h3 className="font-semibold">This is panel header 1</h3>,
+    children: (
+      <span className="text-xs pl-[24px]">
+        A dog is a type of domesticated animal. Known for its loyalty and
+        faithfulness, it can be found as a welcome guest in many households
+        across the world.
+      </span>
+    ),
+  },
+  {
+    key: "2",
+    label: <h3 className="font-semibold">This is panel header 2</h3>,
+    children: (
+      <span className="text-xs pl-[24px]">
+        A dog is a type of domesticated animal. Known for its loyalty and
+        faithfulness, it can be found as a welcome guest in many households
+        across the world.
+      </span>
+    ),
+  },
+  {
+    key: "3",
+    label: <h3 className="font-semibold">This is panel header 3</h3>,
+    children: (
+      <span className="text-xs pl-[24px]">
+        A dog is a type of domesticated animal. Known for its loyalty and
+        faithfulness, it can be found as a welcome guest in many households
+        across the world.
+      </span>
+    ),
+  },
+];
 
 const Test = () => {
   return (
     <div>
-        <CollapsibleMenu />
-        <p>Hello there!</p>
-    </div> 
-  )
-}
+      <Collapse items={items} bordered={false} defaultActiveKey={["1"]} />
+    </div>
+  );
+};
 
-export default Test
+export default Test;
