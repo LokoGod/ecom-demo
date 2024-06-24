18338de769c3cc0a87cebe68423b96a06c08c9b6
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-06-24 21:17:31 +0530

    ui

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -40,9 +40,31 @@ const items: CollapseProps["items"] = [
 
 const Test = () => {
   return (
-    <div>
-      <Collapse items={items} bordered={false} defaultActiveKey={["1"]} />
-    </div>
+    <>
+      <h2 className="poppins-bold text-2xl ml-10 my-3">
+        Longines Legend Diver
+      </h2>
+      <div className="grid grid-cols-4 gap-4 mx-10">
+        <div className="col-span-2">
+          <span className="text-sm">
+            {" "}
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </span>
+        </div>
+
+        <div className="col-span-2">
+          <Collapse items={items} bordered={false} defaultActiveKey={["1"]} />
+        </div>
+      </div>
+    </>
   );
 };
 
