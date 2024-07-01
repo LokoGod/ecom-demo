a42dd36bfca4a8cf3499b8257a9a7793c45d7b3c
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-07-01 22:48:58 +0530

    inital config

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello there!");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import "dotenv/config";
+
+// Import custom routes
+// import { jobRoute } from "./routes/jobRoute";
+
+// Instances
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// API routing
+// app.use("/api/v1/job", jobRoute);
+
+const port = process.env.PORT || 9000;
+
+// Spin-up server
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
