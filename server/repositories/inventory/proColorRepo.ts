46a382232af2b8ffbd0c5cdf226468bbd7ea3677
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-07-16 22:37:38 +0530

    newEndpoint

--- server/repositories/inventory/proColorRepo.ts
+++ server/repositories/inventory/proColorRepo.ts
@@ -11,7 +11,6 @@ const createColor = async (colorName: string, hexCode: string) => {
   })
 }
 
-
 const proColorRepo = {
   getAllColors, createColor
 };
--- server/repositories/inventory/proSizeRepo.ts
+++ server/repositories/inventory/proSizeRepo.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+const getAllSizes = async () => {
+  return prisma.size.findMany();
+};
+
+const proSizeRepo = {
+    getAllSizes
+  };
+  
+  export default proSizeRepo;
+  
