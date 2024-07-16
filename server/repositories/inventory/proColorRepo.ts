b573afc8021280f05196e200ef107a5ddb7d419b
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-07-16 14:14:58 +0530

    getAllColorRepo

--- server/repositories/inventory/proColorRepo.ts
+++ server/repositories/inventory/proColorRepo.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+const getAllColors = async () => {
+  return prisma.colors.findMany();
+};
+
+const proColorRepo = {
+  getAllColors,
+};
+
+export default proColorRepo;
