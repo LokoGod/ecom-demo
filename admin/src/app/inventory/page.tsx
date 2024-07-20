4cb521e20da79f2ca4e2da32249381bc5b65c895
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-07-20 14:55:21 +0530

    com

--- admin/src/app/inventory/page.tsx
+++ admin/src/app/inventory/page.tsx
@@ -1,7 +1,7 @@
 export default function Inventory() {
-    return (
-        <>
-        <h1>Hello Inventory</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Hello Inventory</h1>
+    </>
+  );
 }
