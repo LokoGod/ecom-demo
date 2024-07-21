2f8f7ef71d97ac5ee6ec90607a6ca17b05531525
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-07-21 12:28:11 +0530

    ui

--- admin/src/app/inventory/productColors/page.tsx
+++ admin/src/app/inventory/productColors/page.tsx
@@ -6,12 +6,45 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+
+import Image from "next/image";
+import { BadgePlus, FileSpreadsheet, Filter, MoreHorizontal, SlidersHorizontal } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Boxes, Diamond, Warehouse } from "lucide-react";
+import TestRadarChart from "@/components/charts/inventory/productColor/TestRadarChart";
 
 export default function ProductColors() {
   return (
     <main>
-      <div>
+      <div className="mb-5">
         <Breadcrumb>
           <BreadcrumbList className="text-xs">
             <BreadcrumbItem>
@@ -31,6 +64,126 @@ export default function ProductColors() {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
+      </div>
+
+      <div className="grid grid-cols-4 gap-4">
+        <div className="col-span-2">
+            <TestRadarChart />
+        </div>
+        <div className="col-span-2">
+          <div className="flex justify-end gap-2 mb-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button size="sm" variant="outline" className="mt-0.5 h-8">
+                <Filter size={16} />
+                  <span className="ml-1">Filter</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <Button size="sm" variant="outline" className="mt-0.5 h-8">
+              <FileSpreadsheet size={16} />
+              <span className="ml-1">Export</span>
+            </Button>
+            <Button size="sm">
+              <BadgePlus size={16} />
+              <span className="ml-1">Add color</span>
+            </Button>
+          </div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Colors</CardTitle>
+              <CardDescription>Manage your product colors.</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="hidden w-[100px] sm:table-cell">
+                      <span className="sr-only">Color</span>
+                    </TableHead>
+                    <TableHead>Color Name</TableHead>
+                    <TableHead>Hex Code</TableHead>
+
+                    <TableHead>
+                      <span className="sr-only">Actions</span>
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="hidden sm:table-cell">
+                      <Avatar className="bg-teal-400"></Avatar>
+                    </TableCell>
+                    <TableCell className="italic">Pacific Ocean Blue</TableCell>
+                    <TableCell className="font-semibold">#33ffaf</TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem>Edit</DropdownMenuItem>
+                          <DropdownMenuItem>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+
+                  <TableRow>
+                    <TableCell className="hidden sm:table-cell">
+                      <Avatar className="bg-teal-400"></Avatar>
+                    </TableCell>
+                    <TableCell className="italic">Pacific Ocean Blue</TableCell>
+                    <TableCell className="font-semibold">#33ffaf</TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem>Edit</DropdownMenuItem>
+                          <DropdownMenuItem>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+
+            <CardFooter>
+              <div className="text-xs text-muted-foreground">
+                Showing <strong>1-10</strong> of <strong>32</strong> products
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </main>
   );
