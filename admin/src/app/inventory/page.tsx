251fe45e7a9b354aa939f9a7d63e38c48d144c5d
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-07-21 21:28:13 +0530

    com

--- admin/src/app/inventory/page.tsx
+++ admin/src/app/inventory/page.tsx
@@ -5,12 +5,7 @@ import Tabsbar from "@/components/Tabsbar";
 export default function Inventory() {
   return (
     <main>
-      <h1>Hello Inventory</h1>
-      <Link href={"inventory/productColors"}>
-      <Button>
-        Product Colors
-      </Button>
-      </Link>
+      
       <Tabsbar />
     </main>
   );
